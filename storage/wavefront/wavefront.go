36e5f99fd46bb7097be4ce0696e19d5a205f3a62
Chung Kang Wang <chung@wavefront.com>
2017-06-27 17:15:53 -0700

    remove quotes for well-format tags

--- storage/wavefront/wavefront.go
+++ storage/wavefront/wavefront.go
@@ -1,4 +1,3 @@
-
 // Copyright 2014 Google Inc. All Rights Reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -45,7 +44,7 @@ type wavefrontStorage struct {
 	WfPrefix        string
 	WfTaggifyLabels bool
 	WfLabelFilter   []string
-	WfDebug		bool
+	WfDebug         bool
 	lock            sync.Mutex
 }
 
@@ -72,14 +71,14 @@ const (
 )
 
 var (
-	argProxyAddress  = flag.String("storage_driver_wf_proxy_host", "", "Wavefront Proxy host:port")
-	argPrefix        = flag.String("storage_driver_wf_prefix", "cadvisor.", "Prefix to be added to metrics")
-	argInterval      = flag.Int("storage_driver_wf_interval", 60, "Wavefront flush interval")
-	argAddTags       = flag.String("storage_driver_wf_add_tags", "", "Point tags to add to metrics")
-	argSourceTag     = flag.String("storage_driver_wf_source", "", "Source tag to add to metrics")
+	argProxyAddress = flag.String("storage_driver_wf_proxy_host", "", "Wavefront Proxy host:port")
+	argPrefix = flag.String("storage_driver_wf_prefix", "cadvisor.", "Prefix to be added to metrics")
+	argInterval = flag.Int("storage_driver_wf_interval", 60, "Wavefront flush interval")
+	argAddTags = flag.String("storage_driver_wf_add_tags", "", "Point tags to add to metrics")
+	argSourceTag = flag.String("storage_driver_wf_source", "", "Source tag to add to metrics")
 	argWfLabelFilter = flag.String("storage_driver_wf_label_filter", "", "A comma separated list of labels that should be taggified")
 	argTaggifyLabels = flag.Bool("storage_driver_wf_taggify_labels", true, "If set to true, docker labels will be added as point tags to metrics.")
-	argWfDebug 	 = flag.Bool("storage_driver_wf_debug", true, "If set to true, cAdvisor will log all metric lines.")
+	argWfDebug = flag.Bool("storage_driver_wf_debug", true, "If set to true, cAdvisor will log all metric lines.")
 )
 
 func new() (storage.StorageDriver, error) {
@@ -104,9 +103,8 @@ func newStorage(proxyAddress string, prefix string, interval int, addTags string
 		WfAddTags:       addTags,
 		WfPrefix:        prefix,
 		WfTaggifyLabels: taggifyLabels,
-		WfDebug: 	 wfDebug,
+		WfDebug:         wfDebug,
 	}
-
 
 	glog.Infoln("Initializing Wavefront Storage Driver")
 
@@ -114,7 +112,7 @@ func newStorage(proxyAddress string, prefix string, interval int, addTags string
 	// Parse label filter
 	if labelFilter != "" {
 		wavefrontStorage.WfLabelFilter = strings.Split(labelFilter, ",")
-		glog.Infof("Label filter is set to %s:",labelFilter)
+		glog.Infof("Label filter is set to %s:", labelFilter)
 	}
 
 	// Initialize map that will hold timestamp of the last flush for each container
@@ -132,8 +130,9 @@ func newStorage(proxyAddress string, prefix string, interval int, addTags string
 		if tags != "" {
 			wavefrontStorage.WfAddTags = tags
 		}
-		wavefrontStorage.cleanAddTags()
 	}
+
+	wavefrontStorage.cleanAddTags()
 
 	return wavefrontStorage, nil
 
@@ -163,12 +162,12 @@ func (driver *wavefrontStorage) containerStatsToValues(stats *info.ContainerStat
 func (driver *wavefrontStorage) containerFsStatsToValues(series *map[string]uint64, stats *info.ContainerStats) {
 	for _, fsStat := range stats.Filesystem {
 		// Summary stats.
-		(*series)[colFsSummary+"."+colFsLimit] += fsStat.Limit
-		(*series)[colFsSummary+"."+colFsUsage] += fsStat.Usage
+		(*series)[colFsSummary + "." + colFsLimit] += fsStat.Limit
+		(*series)[colFsSummary + "." + colFsUsage] += fsStat.Usage
 
 		// Per device stats.
-		(*series)[fsStat.Device+"~"+colFsLimit] = fsStat.Limit
-		(*series)[fsStat.Device+"~"+colFsUsage] = fsStat.Usage
+		(*series)[fsStat.Device + "~" + colFsLimit] = fsStat.Limit
+		(*series)[fsStat.Device + "~" + colFsUsage] = fsStat.Usage
 	}
 }
 
@@ -213,9 +212,9 @@ func (driver *wavefrontStorage) AddStats(ref info.ContainerReference, stats *inf
 	source := driver.Source
 
 	//do we need to do any replacements?
-	if strings.Contains(source,"{") && strings.Contains(source, "}") {
-		labelKey := strings.TrimLeft(source,"{")
-		labelKey = strings.TrimRight(labelKey,"}")
+	if strings.Contains(source, "{") && strings.Contains(source, "}") {
+		labelKey := strings.TrimLeft(source, "{")
+		labelKey = strings.TrimRight(labelKey, "}")
 		labelVal := ref.Labels[labelKey]
 		hostname, _ := os.Hostname()
 		if labelVal == "" {
@@ -247,7 +246,7 @@ func (driver *wavefrontStorage) AddStats(ref info.ContainerReference, stats *inf
 			}
 		} else {
 			// else only include labels in the filter
-			for _,v := range driver.WfLabelFilter {
+			for _, v := range driver.WfLabelFilter {
 				if labels[v] != "" {
 					appendTags += " " + v + "=\"" + labels[v] + "\""
 				}
@@ -283,9 +282,56 @@ func (driver *wavefrontStorage) AddStats(ref info.ContainerReference, stats *inf
 func (driver *wavefrontStorage) cleanAddTags() {
 	if strings.HasSuffix(driver.WfAddTags, "\"") {
 		driver.lock.Lock()
-		driver.WfAddTags = strings.TrimRight(driver.WfAddTags,"\"")
+		if shouldRemoveQuote(driver.WfAddTags) {
+			driver.WfAddTags = unquote(driver.WfAddTags)
+		}
 		driver.lock.Unlock()
 	}
+}
+
+// shouldRemoveQuote checks whether the given WfAddTags in wavefrontStorage
+// should be unquoted for proper parsing in WF proxy.  This assumes that
+// the provided string is properly formatted, with only the issues of extraneous quotations.
+// unquote: `"key=value key2=value2"`, to `key=value key2=value2`
+//          `" "key"="value" "key2"="value2"  "` to `"key"="value" "key2"="value2"`
+// but not: `"key"="value" "key2"="value2"`
+func shouldRemoveQuote(s string) bool {
+	n := len(s)
+
+	if n < 2 {
+		return false
+	}
+
+	quote := s[0]
+	if quote != s[n - 1] {
+		return false
+	}
+
+	if quote == '"' {
+		content := s[1 : n - 1]
+		if !strings.Contains(content, "\"") {
+			return true
+		}
+		content = strings.Trim(content, " ")
+
+		content_size := len(content)
+		if content_size >= 2 {
+			if content[0] == '"' && content[0] == content[content_size - 1] {
+				return true
+			}
+		}
+	}
+
+	return false
+}
+
+// unquote removes first and last byte of the param s assuming it is quoted.
+func unquote(s string) (string) {
+	n := len(s)
+	if n < 2 {
+		return s
+	}
+	return s[1 : n - 1]
 }
 
 func (driver *wavefrontStorage) Close() error {
@@ -295,7 +341,7 @@ func (driver *wavefrontStorage) Close() error {
 
 func (driver *wavefrontStorage) Connect() error {
 	// Timeout if unable to connect after 10 seconds.
-	conn, err := net.DialTimeout("tcp", driver.ProxyAddress, time.Second*10)
+	conn, err := net.DialTimeout("tcp", driver.ProxyAddress, time.Second * 10)
 	driver.Conn = conn
 	return err
 }
